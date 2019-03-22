59ab36fbf6d1267204b0adba9485c88175136956
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-03-22 14:41:15 +0100

    Production config and dockerfiles including sources

--- spec/api-spec-test-runner.sh
+++ spec/api-spec-test-runner.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
-set -x
+set -xe
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-APIURL=${APIURL:-http://nginx/api}
-USERNAME=${USERNAME:-u`date +%s`}
-EMAIL=${EMAIL:-$USERNAME@mail.com}
-PASSWORD=${PASSWORD:-password}
 
-npx newman run $SCRIPTDIR/conduit.postman_collection.json \
-  --delay-request 500 \
-  --global-var "APIURL=$APIURL" \
-  --global-var "USERNAME=$USERNAME" \
-  --global-var "EMAIL=$EMAIL" \
-  --global-var "PASSWORD=$PASSWORD"
+while :; do
+    APIURL=${APIURL:-http://nginx/api}
+    USERNAME=${USERNAME:-u`date +%s`}
+    EMAIL=${EMAIL:-$USERNAME@mail.com}
+    PASSWORD=${PASSWORD:-password}
+
+    newman run $SCRIPTDIR/conduit.postman_collection.json \
+        --delay-request 1 \
+        --iteration-count 2 \
+        --verbose \
+        --suppress-exit-code \
+        --reporters cli,json --reporter-json-export /tmp/outputfile.json \
+        --global-var "APIURL=$APIURL" \
+        --global-var "USERNAME=$USERNAME" \
+        --global-var "EMAIL=$EMAIL" \
+        --global-var "PASSWORD=$PASSWORD"
+done
