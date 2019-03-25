c32570d9cc1686ef2a7df878c488d2546c86bb3e
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-03-25 13:26:10 +0100

    Add DD_AGENT_HOST make api_spec_test_runner more resilent

--- spec/api-spec-test-runner.sh
+++ spec/api-spec-test-runner.sh
@@ -5,19 +5,21 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 
 while :; do
-    APIURL=${APIURL:-http://nginx/api}
-    USERNAME=${USERNAME:-u`date +%s`}
-    EMAIL=${EMAIL:-$USERNAME@mail.com}
-    PASSWORD=${PASSWORD:-password}
+    (
+        APIURL=${APIURL:-http://nginx/api}
+        USERNAME=${USERNAME:-u`date +%s`}
+        EMAIL=${EMAIL:-$USERNAME@mail.com}
+        PASSWORD=${PASSWORD:-password}
 
-    newman run $SCRIPTDIR/conduit.postman_collection.json \
-        --delay-request 1 \
-        --iteration-count 2 \
-        --verbose \
-        --suppress-exit-code \
-        --reporters cli,json --reporter-json-export /tmp/outputfile.json \
-        --global-var "APIURL=$APIURL" \
-        --global-var "USERNAME=$USERNAME" \
-        --global-var "EMAIL=$EMAIL" \
-        --global-var "PASSWORD=$PASSWORD"
+        newman run $SCRIPTDIR/conduit.postman_collection.json \
+            --delay-request 1 \
+            --iteration-count 1000 \
+            --verbose \
+            --suppress-exit-code \
+            --reporters cli,json --reporter-json-export /tmp/outputfile.json \
+            --global-var "APIURL=$APIURL" \
+            --global-var "USERNAME=$USERNAME" \
+            --global-var "EMAIL=$EMAIL" \
+            --global-var "PASSWORD=$PASSWORD"
+    )
 done
